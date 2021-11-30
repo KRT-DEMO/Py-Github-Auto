d5b6a961da2b1cbd397f1ac959e95222d832bf9e
KRT-DEMO <95260371+KRT-DEMO@users.noreply.github.com>
2021-11-29 19:05:28 -0700

    Update test_github_api.py

--- test_github_api.py
+++ test_github_api.py
@@ -1,12 +1,12 @@
 import requests
 
-GitHub_API = 'https://api.github.com'
-User_Repos_Resource = '/user/repos'
+GitHub_API = 'https://api.github.com/user/repos'
 headers = {'Accept': 'application/vnd.github.v3+json'}
-#bearertoken = '3f275dd3fa9803072dd1299175f1e10079d0124a'
-bearertoken = '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'
+user = 'KRT-Demo'
+personal_access_token = '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'
 
-response = requests.get(GitHub_API, headers=headers, auth=('Authorization', bearertoken))
+response = requests.get(GitHub_API, headers=headers, auth=(user, personal_access_token))
 
+assert response.status_code == 200
 print(response.status_code)
 print(response.content)
