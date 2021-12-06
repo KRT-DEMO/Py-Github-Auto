bd85be6773aa2f2781303735d10953d69ef8da56
KRT-DEMO <95260371+KRT-DEMO@users.noreply.github.com>
2021-12-06 16:32:59 -0700

    Update test_github_api.py

--- test_github_api.py
+++ test_github_api.py
@@ -4,7 +4,7 @@ GitHub_API = 'https://api.github.com/user/repos'
 headers = {'Accept': 'application/vnd.github.v3+json'}
 user = 'KRT-Demo'
 password = 'Dog.Bone1'
-personal_access_token = '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'
+personal_access_token = ''
 
 response = requests.get(GitHub_API, headers=headers, auth=(user, personal_access_token))
 
